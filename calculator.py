a9febc02e275fbe8edaceba86144bb49217f862c
Chang-Gyu Seong <47137166+cgseong@users.noreply.github.com>
2025-04-10 15:13:31 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,6 +7,9 @@ def subtract(a, b):
     """두 숫자를 뺍니다."""
     return a - b
 
+def multiply(a, b):
+    """두 숫자를 곱합니다."""
+    return a * b
 
 # 간단한 테스트 (터미널에서 python calculator.py 실행 시)
 if __name__ == "__main__":
